29cb47171d81ade44f2d72aac92ec31de655117a
Wei I <n11353091@qut.edu.au>
2023-09-27 11:04:38 +1000

    finish tut week 6

--- travel/views.py
+++ travel/views.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
-
-mainbp = Blueprint('main', __name__)
+from flask import Blueprint, render_template, request, session
+mainbp = Blueprint ('main',__name__)
 
 @mainbp.route('/')
 def index():
     return render_template('index.html')
+
+@mainbp.route('/login')
+def login():
+    email = request.values.get("email")
+    passwd = request.values.get("pwd")
+    print (f"Email: {email}\nPassword: {passwd}")
+    #store email in session
+    session['email'] = request.values.get('email')
+    return render_template('login.html')
+
+@mainbp.route('/logout')
+def logout():
+    if 'email' in session:
+        session.pop('email')
+    return 'User logged out'
